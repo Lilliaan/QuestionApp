e2c49265746a4a1ee837b6e6a4bd585034584958
Lillian Liu <lillianliu42@gmail.com>
2024-07-30 11:13:40 -0400

    Final Question App

---
+++ QuestionApp/QuestionThreeView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionThreeView.swift
+//  QuestionApp
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct QuestionThreeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionThreeView()
+}
